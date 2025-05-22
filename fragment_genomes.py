462044368cfaf254e4ecab929f1abad15b1869ea
Kevin K. <104082976+ktzkvin@users.noreply.github.com>
2025-05-22 11:41:09 +0200

    🔨WIP

--- fragment_genomes.py
+++ fragment_genomes.py
@@ -1,81 +1,53 @@
 #!/usr/bin/env python
-### Credit for script goes to Alex Reynolds on bioinformatics stack exchange https://bioinformatics.stackexchange.com/questions/13879/how-to-fragment-genomes-into-non-overlapping-sequences-of-differing-sizes
-### For fragmenting genomes uniformly
-### ./fragment_genomes <lower bp bound> <higher bp bound> <file_name>
+# -*- coding: utf-8 -*-
+"""
+Credit: Alex Reynolds (Bioinformatics Stack Exchange)
+Adapté pour une exécution modulaire ou directe
+"""
+
 import sys
 import numpy as np
-import timeit
 
-low = int(sys.argv[1])
-high = int(sys.argv[2])
-in_fn = sys.argv[3]
-
-"""
-Globals
-"""
-fragment_lengths = []
-
-"""
-Process a record
-"""
-def process_record(header, sequence, low, high):
-    next = 0
+def process_record(header, sequence, low, high, output=sys.stdout):
+    next_pos = 0
     last = 0
     fragment = 1
-    fragment_sequence = ""
     while last < len(sequence) - high:
-        next = np.random.randint(low, high + 1) + last
-        fragment_sequence += sequence[last:next]
-        sys.stdout.write('{}:{}-{}:{}\n{}\n'.format(header, last, next, fragment, sequence[last:next]))
-        #fragment_lengths.append(next - last)                                                                                                                                                                                                                                                                                                                                                                                            
-        last = next
+        next_pos = np.random.randint(low, high + 1) + last
+        frag_seq = sequence[last:next_pos]
+        output.write(f'{header}:{last}-{next_pos}:{fragment}\n{frag_seq}\n')
+        last = next_pos
         fragment += 1
-    """
-    Uncomment to compare input sequence with fragments
-    """
-    #sys.stderr.write('In > {}\nOut > {}\n'.format(sequence, fragment_sequence))
-    
-"""
-Parse records
-"""
-def parse_records(in_fn, low, high):
+
+def parse_records(in_fn, low, high, output=sys.stdout):
     header = ""
     sequence = ""
     with open(in_fn, 'r') as ifh:
         for line in ifh:
             if line.startswith('>'):
-                if len(sequence) > 0:
-                    process_record(header, sequence, low, high)
+                if sequence:
+                    process_record(header, sequence, low, high, output)
                     sequence = ""
-                header = line.rstrip()
+                header = line.strip()
             else:
-                sequence += line.rstrip()
-        if len(sequence) > 0:
-            process_record(header, sequence, low, high)
+                sequence += line.strip()
+        if sequence:
+            process_record(header, sequence, low, high, output)
 
-def count_elements(seq):
-    hist = {}
-    for i in seq:
-        hist[i] = hist.get(i, 0) + 1
-    return hist
-
-def wrapper(func, *args, **kwargs):
-    def wrapped():
-        return func(*args, **kwargs)
-    return wrapped
-
-def main(in_fn, low, high):
+def fragment_fasta(input_path, low, high):
     """
-    Comment below if testing the distribution of fragments
+    Fonction appelable depuis un script Python.
+    Retourne une fonction qui prend un fichier ouvert en écriture.
     """
-    parse_records(in_fn, low, high)
-    """
-    Uncomment to test the distribution of fragments from 
-    running fns on same input 100k times
-    """
-    #wrapped = wrapper(parse_records, in_fn, low, high)
-    #timeit.timeit(wrapped, number=100000)
-    #print(count_elements(fragment_lengths))
+    return lambda output: parse_records(input_path, low, high, output)
 
 if __name__ == "__main__":
-    main(in_fn, low, high)
+    if len(sys.argv) != 4:
+        print("Usage: python fragment_genomes.py <min_length> <max_length> <input_fasta>")
+        sys.exit(1)
+
+    low = int(sys.argv[1])
+    high = int(sys.argv[2])
+    input_file = sys.argv[3]
+
+    parse_records(input_file, low, high)
